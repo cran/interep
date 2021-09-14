7981ff0047b3482c52abcb6bbe4a05314738ffa0
Fei Zhou <fei.zhou@outlook.com>
2021-09-14 10:20:05 +0000

    version 0.3.2

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# interep 0.3.2 [2021-09-14]
+
+* Fix the error in CRAN package check results.
+* The paper about the method in the package has been added to the reference list.
+* Update reference format.
+* Update Maintainer's contact information.
+
 # interep 0.3.1 [2020-04-19]
 
 * Reference lists have been updated.
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // ScoreU
 Rcpp::List ScoreU(int n, arma::vec& k, arma::vec& y, arma::mat& x, int p, arma::vec& beta, char corre);
 RcppExport SEXP _interep_ScoreU(SEXP nSEXP, SEXP kSEXP, SEXP ySEXP, SEXP xSEXP, SEXP pSEXP, SEXP betaSEXP, SEXP correSEXP) {
